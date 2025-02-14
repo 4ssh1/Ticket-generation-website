f51c61008761905fca1c61991d9dc4e412a6733c
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-02-14 20:25:10 +0100

    redesigned ticket

--- src/Router/DownloadTicket.jsx
+++ src/Router/DownloadTicket.jsx
@@ -11,8 +11,7 @@ function DownloadTicket() {
      navigate("/")
   }
   function handleOption2() {
-    console.log(validatedFormData)
-    
+    return null
   }
 
   return (
@@ -27,34 +26,36 @@ function DownloadTicket() {
           <p>Check your email for a copy or You can <strong>download</strong>.</p>
           <div className=' flex justify-center m-2'>
             <img src="TICKET.svg" alt="Generated ticket" className='ticketheight' />
-            <div className='absolute m-3 activeborder max-w-36 text-xs rounded-md' id='ticketDownload'>
+            <div className='absolute m-3 activeborder max-w-36 text-xs rounded-md h-60' id='ticketDownload'>
               <h2 className='roadFont font-bold'>Techember Fest ” 25</h2>
               <p>📍04 Rumen Road, Ikoyi, Lagos</p>
               <p>📆 March 15, 2025 | 7:00 PM</p>
               <img src={validatedFormData.image} alt="Validated image" className='h-16 block mx-auto rounded-sm mb-1'/>
-              <div>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Your name </th>
-                      <th>Your email *</th>
-                    </tr>
-                    <tr>
-                      <td>{validatedFormData.name}</td>
-                      <td> {validatedFormData.email}</td>
-                    </tr>
-                    <tr>
-                      <th>Tier:</th>
-                      <th>Ticket:</th>
-                    </tr>
-                    <tr>
-                      <td>{ticketData.type}</td>
-                      <td>{ticketData.price}</td>
-                    </tr>
-                  </thead>
-                </table>
+              <div className= 'small pt-2 leading-5'>
+                <div>
+                  <div className='flex justify-between items-start'>
+                    <div>
+                      <p>Name: </p>
+                      <p>Email: </p>
+                    </div>
+                    <div>
+                      <p>{validatedFormData.name}</p>
+                      <p>{validatedFormData.email}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='flex  justify-between'>
+                  <div>
+                    <p>Tier:</p>
+                    <p>Ticket:</p>
+                  </div>
+                  <div>
+                    <p>{ticketData.type}</p>
+                    <p>{ticketData.price}</p>
+                  </div>
+                </div>
               </div>
-            <div className='absolute bg-black translate-y-6 translate-x-4 max-h-12 max-w-28 h-12 w-28' >
+            <div className='absolute max-h-12 max-w-28 h-12 w-28 bottom-0 translate-y-17 translate-x-3' >
               <img src="barCode.jpg" alt="bar-Code" className='w-full h-full' />
             </div>
             </div>
