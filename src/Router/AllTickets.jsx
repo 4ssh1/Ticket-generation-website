4068e4d59e614abc22721267c73dd897df647d4f
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-02-16 20:37:24 +0100

    updated ticket page

--- src/Router/AllTickets.jsx
+++ src/Router/AllTickets.jsx
@@ -1,61 +1,67 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
 import { useNavigate } from 'react-router-dom'
 
 
 function AllTickets() {
   const navigate = useNavigate()
-    const validatedFormData = JSON.parse(localStorage.getItem("UserInfo"))
-    const ticketPrice = JSON.parse(localStorage.getItem("CurrentPrice"))
-    const ticketType = JSON.parse(localStorage.getItem("CurrentTier"))
-  
-   
+  const validatedFormData = JSON.parse(localStorage.getItem("UserInfo"))
+  const ticketPrice = JSON.parse(localStorage.getItem("CurrentPrice"))
+  const ticketType = JSON.parse(localStorage.getItem("CurrentTier"))
+
+
 
   return (
     <div className='bg h-screen md:min-h-[110vh]'>
-         <div>
-            <Navbar />
-         </div>
-         <div className='m-5'>
-            <button className='border-2 border-teal-500 rounded-lg p-3 hover:opacity-20' onClick={()=>navigate('/')}>Back to Home</button>
-         </div>
-         <div className=' flex justify-center'>
-         <img src="TICKET.svg" alt="Generated ticket" className='ticketheight' />
-         <div className='absolute m-3 activeborder max-w-36 text-xs rounded-md' id='ticketDownload'>
-              <h2 className='roadFont font-bold text-lg  text-center'>Techember Fest ” 25</h2>
-              <p>📍04 Rumen Road, Ikoyi, Lagos</p>
-              <p>📆 March 15, 2025 | 7:00 PM</p>
-              <img src={validatedFormData.image} alt="Validated image" className='h-16 block mx-auto rounded-sm mb-1'/>
-              <div className= 'small pt-1 leading-5 px-2'>
-                <div>
-                  <div className='flex justify-between items-start'>
-                    <div>
-                      <p>Name: </p>
-                      <p>Email: </p>
-                    </div>
-                    <div>
-                      <p>{validatedFormData.name}</p>
-                      <p>{validatedFormData.email}</p>
-                    </div>
-                  </div>
-                </div>
-                <div className='flex  justify-between'>
+      <div>
+        <Navbar />
+      </div>
+
+      {!validatedFormData.image || !validatedFormData.name || !validatedFormData.email || !ticketType || !ticketPrice ?
+        <h1 className='text-center text-2xl mt-8'>No ticket available</h1>
+        :
+        <div className=' flex justify-center mt-15'>
+          <img src="TICKET.svg" alt="Generated ticket" className='ticketheight' />
+          <div className='absolute m-3 activeborder max-w-36 text-xs rounded-md' id='ticketDownload'>
+            <h2 className='roadFont font-bold text-lg  text-center'>Techember Fest ” 25</h2>
+            <p>📍04 Rumen Road, Ikoyi, Lagos</p>
+            <p>📆 March 15, 2025 | 7:00 PM</p>
+            <img src={validatedFormData.image} alt="Validated image" className='h-16 block mx-auto rounded-sm mb-1' />
+            <div className='small pt-1 leading-5 px-2'>
+              <div>
+                <div className='flex justify-between items-start'>
                   <div>
-                    <p>Tier:</p>
-                    <p>Ticket:</p>
+                    <p>Name: </p>
+                    <p>Email: </p>
                   </div>
                   <div>
-                    <p>{ticketType}</p>
-                    <p>{ticketPrice}</p>
+                    <p>{validatedFormData.name}</p>
+                    <p>{validatedFormData.email}</p>
                   </div>
                 </div>
               </div>
+              <div className='flex  justify-between'>
+                <div>
+                  <p>Tier:</p>
+                  <p>Ticket:</p>
+                </div>
+                <div>
+                  <p>{ticketType}</p>
+                  <p>{ticketPrice}</p>
+                </div>
+              </div>
+            </div>
             <div className='absolute max-h-12 max-w-28 h-12 w-28 bottom-0 translate-y-16 translate-x-3.5' >
               <img src="barCode.jpg" alt="bar-Code" className='w-full h-full' />
             </div>
-            </div>
+          </div>
         </div>
+      }        
+      <div className='absolute flex justify-center space-x-8 bottom-10 w-full'>
+        <button className='border-2 border-teal-500 rounded-lg p-3 hover:opacity-20' onClick={() => navigate('/')}>Back to Home </button>
+        <button className='border-2 border-teal-500 rounded-lg p-3 hover:opacity-20' onClick={() => navigate('/ticket')}>Ticket Form </button>
+      </div>
     </div>
   )
 }
